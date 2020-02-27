9f4eb307d8b6361eb0e1ec457d1901ebbfaf68b2
Eric VanHeest <eric.van.heest@sap.com>
2020-02-27 15:29:38 -0700

    Dep update

--- components/dispatcher/vendor/github.com/kyma-incubator/knative-kafka/components/common/pkg/kafka/health/constants.go
+++ components/dispatcher/vendor/github.com/kyma-incubator/knative-kafka/components/common/pkg/kafka/health/constants.go
@@ -3,7 +3,6 @@ package health
 // Constants
 const (
 	// Default Health Configuration
-	HealthPort    = 8082        // The Port That Serves The Liveness And Readiness Endpoints
 	LivenessPath  = "/healthz"  // The Endpoint Of The Liveness Check
 	ReadinessPath = "/healthy"  // The Endpoint Of The Readiness Check
 )
