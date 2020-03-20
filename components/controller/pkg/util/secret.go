04a19826571397b378c13f2e22133216f0292d39
Travis Minke <57012088+travis-minke-sap@users.noreply.github.com>
2020-03-20 10:43:58 -0600

    restrict kafka secret informer/lister to knative-eventing namespace (#90)

--- components/controller/pkg/util/secret.go
+++ components/controller/pkg/util/secret.go
@@ -1,13 +1,11 @@
 package util
 
 import (
-	commonconstants "github.com/kyma-incubator/knative-kafka/components/common/pkg/kafka/constants"
 	"github.com/kyma-incubator/knative-kafka/components/controller/constants"
 	"go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"strings"
 )
 
 // Get A Logger With Secret Info
@@ -33,24 +31,5 @@ func NewSecretOwnerReference(secret *corev1.Secret) metav1.OwnerReference {
 		UID:                secret.GetUID(),
 		BlockOwnerDeletion: &blockOwnerDeletion,
 		Controller:         &controller,
-	}
-}
-
-// Custom Filter For Selecting "Kafka" Secrets In knative-eventing Namespace
-func FilterKafkaSecrets() func(obj interface{}) bool {
-	return func(obj interface{}) bool {
-		if object, ok := obj.(metav1.Object); ok {
-			if object.GetNamespace() == constants.KnativeEventingNamespace {
-				labelFound := false
-				for label, value := range object.GetLabels() {
-					if label == commonconstants.KafkaSecretLabel && strings.ToLower(value) == "true" {
-						labelFound = true
-						break
-					}
-				}
-				return labelFound
-			}
-		}
-		return false
 	}
 }
