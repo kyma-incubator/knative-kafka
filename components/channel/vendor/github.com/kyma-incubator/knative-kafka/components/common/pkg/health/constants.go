6fcef1632f81a193cc3f999c69b857b1886d5f4d
travis-minke-sap <travis.minke@sap.com>
2020-04-16 16:15:51 -0600

    first pass, test ok, docker-push ok

--- components/channel/vendor/github.com/kyma-incubator/knative-kafka/components/common/pkg/health/constants.go
+++
@@ -1,8 +0,0 @@
-package health
-
-// Constants
-const (
-	// Default Health Configuration
-	LivenessPath  = "/healthz"  // The Endpoint Of The Liveness Check
-	ReadinessPath = "/healthy"  // The Endpoint Of The Readiness Check
-)
